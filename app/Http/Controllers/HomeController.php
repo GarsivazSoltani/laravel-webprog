822a2ef6e17306c296aa5a6b70cc445789fa46b9
GarsivazSoltani <garsi.soltani@gmail.com>
2024-01-03 22:27:23 -0800

    part47-db-crud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,25 @@ class HomeController extends Controller
     // }
 
     public function home(){
-        $articles = DB::table('articles')->where('id', '=', 1)->get();
+        // $articles = DB::table('articles')->where('id', '=', 1)->get();
         // dd($result);
-        return view('index', compact('articles'));
+        // return view('index', compact('articles'));
+
+        // $result = DB::table('articles')->insert([
+        //     ['title' => 'Title 3', 'body' => 'Lorem ipsum'],
+        //     ['title' => 'Title 4', 'body' => 'Lorem ipsum']
+        // ]);
+
+        // $result = 
+        //     DB::table('articles')
+        //     ->where('id', '=', 4)
+        //     ->update(['title' => 'title 44', 'body' => 'Lorem 4']);
+
+        $result = 
+            DB::table('articles')
+            ->where('id', 4)
+            ->delete();
+        
+        dd($result);
     }
 }
