b12c7ca0846633435a7ca33b262b97c5a5a7680f
GarsivazSoltani <garsi.soltani@gmail.com>
2024-01-03 22:47:00 -0800

    part49-create-model-article

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,25 +14,15 @@ class HomeController extends Controller
     // }
 
     public function home(){
-        // $articles = DB::table('articles')->where('id', '=', 1)->get();
-        // dd($result);
-        // return view('index', compact('articles'));
-
-        // $result = DB::table('articles')->insert([
-        //     ['title' => 'Title 3', 'body' => 'Lorem ipsum'],
-        //     ['title' => 'Title 4', 'body' => 'Lorem ipsum']
-        // ]);
-
         // $result = 
         //     DB::table('articles')
-        //     ->where('id', '=', 4)
-        //     ->update(['title' => 'title 44', 'body' => 'Lorem 4']);
-
-        $result = 
-            DB::table('articles')
-            ->where('id', 4)
-            ->delete();
+        //     ->where('id', 4)
+        //     ->delete();
         
-        dd($result);
+        $articles = Article::all();
+
+        // dd($result);
+
+        return view('index', compact('articles'));
     }
 }
